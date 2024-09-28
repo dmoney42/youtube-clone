779bb7361522ba07bc015b9b3c45afbb830f54d8
Demetrius Sullivan <dssullivan1@gmail.com>
2024-09-27 21:29:44 -0400

    2nd commit we finished the comment and publisher section of the video page

--- src/Components/PlayVideo/playvideo.jsx
+++ src/Components/PlayVideo/playvideo.jsx
@@ -18,10 +18,106 @@ const playvideo = () => {
     
        <h3>Best Youtube Channel To Lean Web Development</h3>
 
-       <div className="play-video-info">
-        <p>1525 Views &bull; 2 days ago</p>
-       </div>
+        <div className="play-video-info">
+          <p>1525 Views &bull; 2 days ago</p>
+            <div>
+              <span><img src={like} alt=""/> 125</span>
+              <span><img src={dislike} alt=""/> 2</span>
+              <span><img src={share} alt=""/> Share</span>
+              <span><img src={save} alt=""/> save</span>
+            </div>
+        </div>{/* end of play-video-info */}
+
+        <hr/>
+
+        <div className="publisher">
+          <img src={jack} alt="" />
+          <div>
+            <p>Greatstack</p>
+            <span>1M Subscribers</span>
+          </div>{/*channel name div */}
+          {/***************** */}
+          <button>Subscribe</button>
+
+        </div>{/* end publisher div */}
+
+        <div className="vid-description">
+          <p>Channel that makes learning easy</p>
+          <p>Subscribe to GreatStack to Watch More Tutorials on web development</p>
+          <hr />
+          <h4>130 Comments</h4>
+
+              <div className="comment">
+                <img src={user_profile} alt="" />
+                <div name="userComment">
+                  <h3>Jack Nicholson <span>1 day ago</span></h3>
+                  <p>Awesome video. Very helpful. I just subscribed.</p>
+                    <div className="comment-action">
+                      <img src={like} alt="" />
+                      <span>244</span>
+                      <img src={dislike} alt="" />
+                    </div>
+                </div>{/* end userComment */}
+              </div>{/* end comment div */}
+
+              <div className="comment">
+                <img src={user_profile} alt="" />
+                <div name="userComment">
+                  <h3>Jack Nicholson <span>1 day ago</span></h3>
+                  <p>Awesome video. Very helpful. I just subscribed.</p>
+                    <div className="comment-action">
+                      <img src={like} alt="" />
+                      <span>244</span>
+                      <img src={dislike} alt="" />
+                    </div>
+                </div>{/* end userComment */}
+              </div>{/* end comment div */}
+
+              <div className="comment">
+                <img src={user_profile} alt="" />
+                <div name="userComment">
+                  <h3>Jack Nicholson <span>1 day ago</span></h3>
+                  <p>Awesome video. Very helpful. I just subscribed.</p>
+                    <div className="comment-action">
+                      <img src={like} alt="" />
+                      <span>244</span>
+                      <img src={dislike} alt="" />
+                    </div>
+                </div>{/* end userComment */}
+              </div>{/* end comment div */}
+
+              <div className="comment">
+                <img src={user_profile} alt="" />
+                <div name="userComment">
+                  <h3>Jack Nicholson <span>1 day ago</span></h3>
+                  <p>Awesome video. Very helpful. I just subscribed.</p>
+                    <div className="comment-action">
+                      <img src={like} alt="" />
+                      <span>244</span>
+                      <img src={dislike} alt="" />
+                    </div>
+                </div>{/* end userComment */}
+              </div>{/* end comment div */}
+
+
+              <div className="comment">
+                <img src={user_profile} alt="" />
+                <div name="userComment">
+                  <h3>Jack Nicholson <span>1 day ago</span></h3>
+                  <p>Awesome video. Very helpful. I just subscribed.</p>
+                    <div className="comment-action">
+                      <img src={like} alt="" />
+                      <span>244</span>
+                      <img src={dislike} alt="" />
+                    </div>
+                </div>{/* end userComment */}
+              </div>{/* end comment div */}
+
+        </div>{/*end vid-description */}
+
     </div>/* end of play-video div */
+
+  
   )
 }
 
