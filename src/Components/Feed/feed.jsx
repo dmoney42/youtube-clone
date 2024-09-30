cbc53025b7c62d9281db5d93706fef60a2c590cb
Demetrius Sullivan <dssullivan1@gmail.com>
2024-09-30 13:45:57 -0400

    integrated youtube api on the feed.jsx page to display youtube video grid

--- src/Components/Feed/feed.jsx
+++ src/Components/Feed/feed.jsx
@@ -17,8 +17,10 @@ const feed = ({category}) => {
      const [data,setData] = useState([]);
 
      const fetchData = async () => {
-        const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?maxResults=50&videoCategoryId=${category}&key=${API_KEY}`;
-        await fetch(videoList_url).then(response=>response.json()).then(data=>setData(data.items))
+        const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?chart=mostPopular&maxResults=50&videoCategoryId=0&key=${API_KEY}`;
+        await fetch(videoList_url)
+        .then(response=>response.json())
+        .then(data=>setData(data.items))
      } 
 
      useEffect(()=>{
@@ -37,22 +39,19 @@ const feed = ({category}) => {
        
 
             
-        {/*
+            
             {data.map((item,index)=>{
                 return(
-                
+                <Link to={`video/20/4521`} className='card'>
+                    <img src={thumbnail1} alt="" />
+                    <h2>Best channel to learn coding that will help you to be a web developer</h2>
+                    <h3>Greatstack</h3>
+                    <p>15k views &bull; 2 days ago</p>
+                </Link>  
                 )
             })}
-        
-        */}
-
-        <Link to={`video/20/4521`} className='card'>
-            <img src={thumbnail1} alt="" />
-            <h2>Best channel to learn coding that will help you to be a web developer</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </Link>
-
+            
+        {/*  
         <div className='card'>
             <img src={thumbnail2} alt="" />
             <h2>Best channel to learn coding that will help you to be a web developer</h2>
@@ -156,7 +155,7 @@ const feed = ({category}) => {
             <h3>Greatstack</h3>
             <p>15k views &bull; 2 days ago</p>
         </div>
-    
+        */}  
 
     </div>/* end of 'feed' div */
 
