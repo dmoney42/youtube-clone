c6176006c5829e12ff1ad5eea376d273f3794ef0
Demetrius Sullivan <dssullivan1@gmail.com>
2024-10-05 16:00:02 -0400

    added more youtube api links to the playvideo.jsx file to pull video title, subscriber count channel name, channel logo, channel description, comment count, view count, how long ago the video was uploaded

--- src/Components/PlayVideo/playvideo.jsx
+++ src/Components/PlayVideo/playvideo.jsx
@@ -7,9 +7,40 @@ import share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
+import { API_KEY, value_converter } from '../../data';
+import { useState } from 'react'; /*we have to import useState hook from react to be able to use the useState() function*/
+import { useEffect } from 'react'; /* we have to import useEffect hook from react to be able to use the useEffect() function*/
+import moment from 'moment';
+
+  const playvideo = ({videoId}) => {
+
+   const [apiData,setApiData] = useState(null);
+   const [channelData,setChannelData] = useState(null);
+
+   const fetchVideoData = async () => {
+      //Fetching Video's data
+      const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`
+      await fetch(videoDetails_url).then(res=>res.json()).then(data => setApiData(data.items[0]));
+    } 
 
 
-const playvideo = ({videoId}) => {
+    const fetchOtherData = async () => {
+      //fetching channel data
+      const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`
+      await fetch(channelData_url).then(res=>res.json()).then(data=>setChannelData(data.items[0]))
+    }
+
+    
+    useEffect(()=>{
+
+      fetchVideoData();
+
+    },[])
+
+    useEffect(()=>{
+      fetchOtherData();
+    },[apiData])
+    
 
   return (
     <div className='play-video'>
@@ -18,13 +49,24 @@ const playvideo = ({videoId}) => {
         </video>*/}
         <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     
-       <h3>Best Youtube Channel To Lean Web Development</h3>
+      
+      {/*The <h3> statement checks if apiData, apiData.snippet, and apiData.snippet.title exist.
+       If they do, it displays the title; if not, it shows "Title not pulled from API." 
+       This prevents errors by ensuring each part is defined before accessing the next. */}
+       <h3>
+       {apiData && apiData.snippet && apiData.snippet.title 
+        ? apiData.snippet.title 
+        : "Title not pulled from API"}
+       </h3>
 
+       {/* <h3>Default title without youtube api</h3> */}
+        
         <div className="play-video-info">
-          <p>1525 Views &bull; 2 days ago</p>
+          
+          <p>{apiData?value_converter(apiData.statistics.viewCount):"View count not pulled from youtube api"} Views &bull; {apiData?moment(apiData.snippet.publishedAt).fromNow():"upload time not pulled from youtube api"}</p>
             <div>
-              <span><img src={like} alt=""/> 125</span>
-              <span><img src={dislike} alt=""/> 2</span>
+              <span><img src={like} alt=""/> {apiData?value_converter(apiData.statistics.likeCount):"like count not pulled from youtube api"}</span>
+              <span><img src={dislike} alt=""/></span> {/*we cant pull dislikes from youtube api because youtube not longer displays dislikes */}
               <span><img src={share} alt=""/> Share</span>
               <span><img src={save} alt=""/> save</span>
             </div>
@@ -33,10 +75,11 @@ const playvideo = ({videoId}) => {
         <hr/>
 
         <div className="publisher">
-          <img src={jack} alt="" />
+          <img src={channelData?channelData.snippet.thumbnails.default.url:"channel logo not pulled from youtube api"} alt="" />
           <div>
-            <p>Greatstack</p>
-            <span>1M Subscribers</span>
+            <p>{apiData?apiData.snippet.channelTitle:"Channel title not pulled from youtube api"}</p>
+            <span>{channelData?value_converter(channelData.statistics.subscriberCount):"subscriber count not pulled from youtube api"} Subscribers</span> {/* to dynamcially pull the channel subscribers and channel image
+            we have to create another youtube api link under the channels api section of youtube api */}
           </div>{/*channel name div */}
           {/***************** */}
           <button>Subscribe</button>
@@ -44,11 +87,13 @@ const playvideo = ({videoId}) => {
         </div>{/* end publisher div */}
 
         <div className="vid-description">
+          <p>{apiData?apiData.snippet.description.slice(0,250):"description not pulled from youtube api"}</p>
           <p>Channel that makes learning easy</p>
           <p>Subscribe to GreatStack to Watch More Tutorials on web development</p>
           <hr />
-          <h4>130 Comments</h4>
-
+          <h4>{apiData?value_converter(apiData.statistics.commentCount):"comment count not pulled from youtube api"} Comments</h4>
+              {/* to dynamcially pull the comments under a videi
+            we have to create another youtube api link under the commentThreads api section of youtube api */}
               <div className="comment">
                 <img src={user_profile} alt="" />
                 <div name="userComment">
